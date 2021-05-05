cbe87971c8d92a564c466576dd1f5d8d3966c565
alvin almodal <almodalalvin@gmail.com>
2021-05-05 19:38:01 +0800

    test fix for heroku deployment

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ let productRepo = new ProductRepository(mongoose);
 
 const app = express();
 
-try {
-  const db = await mongoose.connect("mongodb://localhost/e-commerce");
-  console.log("Connected to mongodb...");
-} catch (error) {
-  console.log("Unable to connect to mongodb.", error);
-}
+// try {
+//   const db = await mongoose.connect("mongodb://localhost/e-commerce");
+//   console.log("Connected to mongodb...");
+// } catch (error) {
+//   console.log("Unable to connect to mongodb.", error);
+// }
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get("/api/products", async (req, res) => {
-  let products = {};
-  let errors = {};
-  try {
-    products = await productRepo.getProducts();
-  } catch (error) {
-    errors = error;
-    console.log("Failed to get products", error);
-  }
-  res.send({ data: products, errors });
-});
+// app.get("/api/products", async (req, res) => {
+//   let products = {};
+//   let errors = {};
+//   try {
+//     products = await productRepo.getProducts();
+//   } catch (error) {
+//     errors = error;
+//     console.log("Failed to get products", error);
+//   }
+//   res.send({ data: products, errors });
+// });
 
 const port = process.env.PORT || 3001;
 
