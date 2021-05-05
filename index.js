65373a61344e4943e809a2657cb84e05fdba969e
alvin almodal <almodalalvin@gmail.com>
2021-05-05 20:25:43 +0800

    added mongodb dev url

--- index.js
+++ index.js
@@ -5,30 +5,45 @@ let productRepo = new ProductRepository(mongoose);
 
 const app = express();
 
-// try {
-//   const db = await mongoose.connect("mongodb://localhost/e-commerce");
-//   console.log("Connected to mongodb...");
-// } catch (error) {
-//   console.log("Unable to connect to mongodb.", error);
-// }
+app.use(express.json());
+
+try {
+  const db = await mongoose.connect(
+    process.env.MONGO_DB || "mongodb://localhost/e-commerce"
+  );
+  console.log("Connected to mongodb...");
+} catch (error) {
+  console.log("Unable to connect to mongodb.", error);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// app.get("/api/products", async (req, res) => {
-//   let products = {};
-//   let errors = {};
-//   try {
-//     products = await productRepo.getProducts();
-//   } catch (error) {
-//     errors = error;
-//     console.log("Failed to get products", error);
-//   }
-//   res.send({ data: products, errors });
-// });
+app.get("/api/products", async (req, res) => {
+  let products = {};
+  let errors = {};
+  try {
+    products = await productRepo.getProducts();
+  } catch (error) {
+    errors = error;
+    console.log("Failed to get products", error);
+  }
+  res.send({ data: products, errors });
+});
 
-const port = process.env.PORT || 3001;
+app.post("/api/products", async (req, res) => {
+  let result = {};
+  let errors = {};
+  try {
+    result = await productRepo.save(req.body);
+  } catch (error) {
+    errors = error;
+  }
+  res.send({ data: result, errors });
+});
+
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Listening in port ${port}`);
